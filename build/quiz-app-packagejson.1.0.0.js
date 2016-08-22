d6b8f05593a8a6d735cecd2da1fd086a66b61054
R. Terrado <r.terrado@gmail.com>
2016-08-22 13:11:49 -0700

    JS Babel and Uglify

--- build/quiz-app-packagejson.1.0.0.js
+++ build/quiz-app-packagejson.1.0.0.js
@@ -44,48 +44,25 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	var QUESTIONS = [
-	    {
-	        text: 'What sound does a cow make?',
-	        answers: [
-	            'Moo',
-	            'Woof',
-	            'Quack',
-	            'Cheep'
-	        ],
-	        correct: 0
-	    },
-	    {
-	        text: 'What sound does a chicken make?',
-	        answers: [
-	            'Meow',
-	            'Cluck',
-	            'Baa',
-	            'Bleat'
-	        ],
-	        correct: 1
-	    },
-	    {
-	        text: 'What sound does a pig make?',
-	        answers: [
-	            'Woof',
-	            'Howl',
-	            'Oink',
-	            'Moo'
-	        ],
-	        correct: 2
-	    },
-	    {
-	        text: 'What sound does a duck make?',
-	        answers: [
-	            'Oink',
-	            'Meow',
-	            'Honk',
-	            'Quack'
-	        ],
-	        correct: 3
-	    }
-	];
+	'use strict';
+	
+	var QUESTIONS = [{
+	    text: 'What sound does a cow make?',
+	    answers: ['Moo', 'Woof', 'Quack', 'Cheep'],
+	    correct: 0
+	}, {
+	    text: 'What sound does a chicken make?',
+	    answers: ['Meow', 'Cluck', 'Baa', 'Bleat'],
+	    correct: 1
+	}, {
+	    text: 'What sound does a pig make?',
+	    answers: ['Woof', 'Howl', 'Oink', 'Moo'],
+	    correct: 2
+	}, {
+	    text: 'What sound does a duck make?',
+	    answers: ['Oink', 'Meow', 'Honk', 'Quack'],
+	    correct: 3
+	}];
 	
 	var questionsPageElement = $('.questions-page');
 	var questionCurrentElement = $('.question-current');
@@ -97,37 +74,37 @@
 	var scoreElement = $('.score');
 	var restartButtonElement = $('.restart-button');
 	
-	var showResults = function() {
+	var showResults = function showResults() {
 	    questionsPageElement.hide();
 	    resultsPageElement.show();
 	};
 	
-	var showQuestions = function() {
+	var showQuestions = function showQuestions() {
 	    resultsPageElement.hide();
 	    questionsPageElement.show();
 	};
 	
-	var resetScore = function() {
+	var resetScore = function resetScore() {
 	    scoreElement.text(0);
 	};
 	
-	var increaseScore = function() {
+	var increaseScore = function increaseScore() {
 	    var score = parseInt(scoreElement.text(), 10);
 	    scoreElement.text(score + 1);
 	};
 	
-	var setQuestion = function(questionIndex) {
+	var setQuestion = function setQuestion(questionIndex) {
 	    var question = QUESTIONS[questionIndex];
 	    questionCurrentElement.text(questionIndex);
 	    questionElement.text(question.text);
 	    answersElement.empty();
-	    for (var i=0; i<question.answers.length; i++) {
+	    for (var i = 0; i < question.answers.length; i++) {
 	        var answer = question.answers[i];
 	        answersElement.append('<li><button type="button">' + answer + '</button></li>');
 	    }
 	};
 	
-	answersElement.on('click', 'button', function() {
+	answersElement.on('click', 'button', function () {
 	    var choice = $(this).parent().index();
 	    var questionIndex = parseInt(questionCurrentElement.text(), 10);
 	    var question = QUESTIONS[questionIndex];
@@ -137,23 +114,21 @@
 	
 	    if (questionIndex + 1 < QUESTIONS.length) {
 	        setQuestion(questionIndex + 1);
-	    }
-	    else {
+	    } else {
 	        showResults();
 	    }
 	});
 	
-	restartButtonElement.click(function() {
+	restartButtonElement.click(function () {
 	    setQuestion(0);
 	    resetScore();
 	    showQuestions();
 	});
 	
-	$(document).ready(function() {
+	$(document).ready(function () {
 	    questionsTotalElement.text(QUESTIONS.length);
 	    setQuestion(0);
 	});
-
 
 /***/ }
 /******/ ]);
